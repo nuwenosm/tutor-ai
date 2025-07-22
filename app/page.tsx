23508450b6281bdf94f4cee2811047334f9826c0
nuwenosm <nuwenosm@gmail.com>
2025-07-22 11:23:21 -0700

    Setup Routes

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl underline">
+        Weclome to my saas
+      </h1>
+      <Button>Let's get started</Button>
+    </div>
+  )
+}
+
+export default Page
